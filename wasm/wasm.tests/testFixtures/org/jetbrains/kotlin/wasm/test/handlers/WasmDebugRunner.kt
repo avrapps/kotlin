28eed4c7bdd4d0b4f6f1c14435ee53f09b35f8e4
Jim M. R. Teichgräber <jim.teichgraeber@jetbrains.com>
2026-02-02 15:43:48 +0100

    [Wasm][JS] Support local variable tests on K/Wasm (KT-83245)

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/handlers/WasmDebugRunner.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/handlers/WasmDebugRunner.kt
@@ -11,7 +11,11 @@ import org.jetbrains.kotlin.test.model.WasmCompilationSetsBinaryArtifact
 import org.jetbrains.kotlin.test.services.TestServices
 import java.io.File
 
-class WasmDebugRunner(testServices: TestServices) : WasmDebugRunnerBase(testServices) {
+// TODO check correctness of rebase
+
+// TODO(review) move WasmDebugRunner{Base,WithPrecompiled} to this file? Multiple files are confusing/not helpful
+open class WasmDebugRunner(testServices: TestServices, includeLocalVariableInformation: Boolean = false) :
+    WasmDebugRunnerBase(testServices, includeLocalVariableInformation) {
     private fun processCompilationSet(compilationSet: WasmCompilationSet, mode: String) {
         val outputDirBase = testServices.getWasmTestOutputDirectory()
         val devDir = File(outputDirBase, mode)
@@ -39,3 +43,6 @@ class WasmDebugRunner(testServices: TestServices) : WasmDebugRunnerBase(testServ
         }
     }
 }
+
+class WasmLocalVariableDebugRunner(testServices: TestServices) :
+        WasmDebugRunner(testServices, includeLocalVariableInformation = true)
